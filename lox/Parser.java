49b80710ea5dcfde095823bdeaa81715970e8868
Sebastjan <sebastjancizel@gmail.com>
2022-02-27 21:57:25 +0000

    WIP

--- lox/Parser.java
+++ lox/Parser.java
@@ -26,6 +26,71 @@ public class Parser {
 		return expr;
 	}
 
+	private Expr comparison(){
+		Expr expr = term();
+
+		while (match(LESS, GREATER, LESS_EQUAL, GREATER_EQUAL)){
+			Token operator = previous();
+			Expr right = term();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+
+		return expr;
+
+	}
+
+	private Expr term(){
+		Expr expr = factor();
+
+		while (match(MINUS, PLUS)){
+			Token operator = previous();
+			Expr right = factor();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+
+		return expr;
+	}
+
+	private Expr factor() {
+		Expr expr = unary();
+
+		while (match(SLASH, STAR)) {
+			Token operator = previous();
+			Expr right = factor();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+
+		return expr;
+	}
+
+	private Expr unary() {
+		if (match(BANG, MINUS)) {
+			Token operator = previous();
+			Expr right = unary();
+			return new Expr.Unary(operator, right);
+		}
+
+		return primary();
+	}
+
+
+	private Expr primary() {
+		if (match(FALSE)) return new Expr.Literal(false);
+		if (match(TRUE)) return new Expr.Literal(true);
+		if (match(NIL)) return new Expr.Literal(null);
+
+		if (match(NUMBER, STRING)) {
+			return new Expr.Literal(previous().literal);
+		}
+
+		if (match(LEFT_PAREN)) {
+			Expr expr = expression();
+			consume(RIGHT_PAREN, "Expect ')' after expression.");
+			return new Expr.Grouping(expr);
+		}
+
+	}
+
 	private boolean match(TokenType... types){
 		for(TokenType type: types){
 			if(check(type)){
@@ -35,6 +100,17 @@ public class Parser {
 		}
 		return false;
 	}
+
+	private Token consume(TokenType type, String message) {
+		if (check(type)) return advance();
+		return error(peek(), message);
+	}
+
+	private ParseError error(Token token, String message) {
+		Lox.error(token, message);
+		return new ParseError();
+	}
+
 
 	private boolean check(TokenType type){
 		if (isAtEnd()) return false;
