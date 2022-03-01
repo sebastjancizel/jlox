a81f37de3dc95bbaf0929e9a62b80de266e569a8
Sebastjan <sebastjancizel@gmail.com>
2022-03-01 22:58:10 +0000

    Adds inital work on Interpreter

---
+++ lox/Interpreter.java
@@ -0,0 +1,43 @@
+package lox;
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+	@Override
+	public Object visitLiteralExpr(Expr.Literal expr) {
+		return expr.value;
+	}
+
+	@Override
+	public Object visitUnaryExpr(Expr.Unary expr) {
+		Object right = evaluate(expr.right);
+
+		switch (expr.operator.type) {
+			case BANG:
+				return !isTruthy(right);
+			case MINUS:
+				return -(double) right;
+		}
+
+		return null;
+	}
+
+	private boolean isTruthy(Object object) {
+		if (object == null)
+			return false;
+		if (object instanceof Boolean)
+			return (boolean) object;
+		return true;
+	}
+
+	@Override
+	public Object visitGroupingExpr(Expr.Grouping expr) {
+		return evaluate(expr.expression);
+	}
+
+	private Object evaluate(Expr expr){
+		return expr.accept(this);
+	}
+
+
+
+}
